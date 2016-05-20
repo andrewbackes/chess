6c3211189e788f54d457e7458b2137656439fce1
Andrew Backes <rbackes@ucla.edu>
2016-05-19 21:07:15 -0700

    additional test

--- filter_test.go
+++ filter_test.go
@@ -4,16 +4,51 @@ import (
 	"testing"
 )
 
-func TestFilter(t *testing.T) {
+func testPGNs() []*PGN {
 	pgns := []*PGN{
-		&PGN{Tags: map[string]string{"Event": "keep"}},
-		&PGN{Tags: map[string]string{"Event": "remove"}},
-		&PGN{Tags: map[string]string{"Event": "keep"}},
+		&PGN{Tags: map[string]string{
+			"Event": "keep", "WhiteElo": "2200", "BlackElo": "2700", "Round": "1"}},
+		&PGN{Tags: map[string]string{
+			"Event": "remove", "WhiteElo": "2400", "BlackElo": "2500", "Round": "2"}},
+		&PGN{Tags: map[string]string{
+			"Event": "keep", "WhiteElo": "2600", "BlackElo": "2300", "Round": "3"}},
 	}
+	return pgns
+}
+
+func TestFilterEquals(t *testing.T) {
+	pgns := testPGNs()
 	expected := pgns[1]
 	filtered := FilterPGNs(pgns, NewTagFilter("Event==remove"))
 	if len(filtered) != 1 || filtered[0] != expected {
 		t.Log(filtered)
+		t.Fail()
+	}
+}
+
+func TestFilterNotEquals(t *testing.T) {
+	pgns := testPGNs()
+	filtered := FilterPGNs(pgns, NewTagFilter("Event!=remove"))
+	if len(filtered) != 2 || filtered[0].Tags["Round"] != "1" || filtered[1].Tags["Round"] != "3" {
+		t.Log(filtered)
+		t.Fail()
+	}
+}
+
+func TestFiltGT(t *testing.T) {
+	pgns := testPGNs()
+	f := NewTagFilter("BlackElo>2500")
+	filtered := FilterPGNs(pgns, f)
+	if len(filtered) != 1 || filtered[0].Tags["Round"] != "1" {
+		t.Fail()
+	}
+}
+
+func TestFiltGTEQ(t *testing.T) {
+	pgns := testPGNs()
+	f := NewTagFilter("BlackElo>=2500")
+	filtered := FilterPGNs(pgns, f)
+	if len(filtered) != 2 || filtered[0].Tags["Round"] != "1" || filtered[1].Tags["Round"] != "2" {
 		t.Fail()
 	}
 }

7c14db4d65fbf39b19ac7cc98f09859472a33b62
Andrew Backes <rbackes@ucla.edu>
2016-06-05 10:38:24 -0700

    do not allow partial variations in book

--- book/pgn.go
+++ book/pgn.go
@@ -9,7 +9,9 @@ import (
 )
 
 // FromPGN creates an opening book from a PGN. 'depth' is the number of plies
-// to include in the opening book.
+// to include in the opening book. Variations that arent deep enough aren't
+// included. Meaning, if you specify depth 14 but all of the games in your
+// pgn only go to depth 10, then your book will be empty.
 func FromPGN(pgns []*pgn.PGN, depth int) (*Book, error) {
 	if len(pgns) == 0 {
 		return nil, errors.New("no games in pgn")
@@ -17,7 +19,7 @@ func FromPGN(pgns []*pgn.PGN, depth int) (*Book, error) {
 	book := New()
 	for _, pgn := range pgns {
 		// skip games where we don't know the opening moves
-		if pgn.Tags["FEN"] == "" {
+		if pgn.Tags["FEN"] == "" || pgn.Tags["FEN"] == "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" {
 			book.addPgn(pgn, depth)
 		}
 	}
@@ -26,13 +28,21 @@ func FromPGN(pgns []*pgn.PGN, depth int) (*Book, error) {
 
 func (b *Book) addPgn(pgn *pgn.PGN, depth int) {
 	g := game.New()
+	type pair struct {
+		key  uint64
+		move position.Move
+	}
+	var staged []pair
 	for d, m := range pgn.Moves {
 		if d >= depth {
+			for _, s := range staged {
+				b.addMove(s.key, s.move)
+			}
 			return
 		}
 		if mv, err := g.Position.ParseMove(m); err == nil {
 			key := polyglot.Encode(g.Position)
-			b.addMove(key, mv)
+			staged = append(staged, pair{key, mv})
 			status := g.MakeMove(mv)
 			if status != game.InProgress {
 				return
--- book/pgn_test.go
+++ book/pgn_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 )
 
+/*
 func TestFromPGN(t *testing.T) {
 	input := `[Event "one"]
 [Round "1"]
@@ -24,4 +25,27 @@ func TestFromPGN(t *testing.T) {
 	pgns, _ := pgn.Open(strings.NewReader(input))
 	book, _ := FromPGN(pgns, 4)
 	fmt.Println(book)
+}
+*/
+
+func TestCorrectNumber(t *testing.T) {
+	input := `[Event "one"]
+[Round "1"]
+[Result "1-0"]
+
+1. e2e4 e7e5 2. d1h5 e8e7 3. h5e5 1-0
+`
+	pgns, _ := pgn.Open(strings.NewReader(input))
+	book, _ := FromPGN(pgns, 4)
+
+	if len(book.Positions) != 4 {
+		t.Log(book)
+		t.Fail()
+	}
+	for _, m := range book.Positions {
+		if len(m) != 1 {
+			t.Log(book)
+			t.Fail()
+		}
+	}
 }
